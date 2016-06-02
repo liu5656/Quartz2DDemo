ac297bf83be530ae34731f88dfab781d1ae5770f
liu5656 <lj@liujiandeimac.local>
2016-06-02 13:38:40 +0800

    Initial Commit

---
+++ Quartz2DDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quartz2DDemo
+//
+//  Created by 刘健 on 16/6/2.
+//  Copyright © 2016年 刘健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quartz2DDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quartz2DDemo
+//
+//  Created by 刘健 on 16/6/2.
+//  Copyright © 2016年 刘健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
